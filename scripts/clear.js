57e534db5f34005dfff950b2209c4155eb01ecb5
Aguus467 <nahuelmar7@gmail.com>
2025-11-11 21:00:23 -0300

    Add files via upload

---
+++ scripts/clear.js
@@ -0,0 +1,9 @@
+// Limpia la consola cada 2 segundos
+function limpiarConsolaConstante() {
+    setInterval(() => {
+        console.clear();
+    }, 1000); // 2000ms = 2 segundos
+}
+
+// Iniciar la limpieza automática
+limpiarConsolaConstante();
