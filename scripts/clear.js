95fada8ac6b5b4df094e4489a9b51d399df07375
Aguus467 <nahuelmar7@gmail.com>
2025-11-15 14:47:11 -0300

    Update clear.js

--- scripts/clear.js
+++ scripts/clear.js
@@ -1,9 +1,1 @@
-// Limpia la consola cada 2 segundos
-function limpiarConsolaConstante() {
-    setInterval(() => {
-        console.clear();
-    }, 1000); // 2000ms = 2 segundos
-}
 
-// Iniciar la limpieza automática
-limpiarConsolaConstante();
